c2915d04ab10d160221c9075ecb427ebb67ecfd3
petermunene <peter.munene@student.moringaschool.com>
2025-06-27 19:46:31 +0300

    deployment

--- server/app.py
+++ server/app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.secret_key = "muriukimunene"
 CORS(app, supports_credentials=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user2:muriukimunene@localhost:5432/ecommerce_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
